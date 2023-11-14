a7d707bc09bf2b910415e84eef065a0681a2f153
jadentyl-r <150844233+jadentyl-r@users.noreply.github.com>
2023-11-14 13:30:07 -0500

    flip boo

--- gittest.ino
+++ gittest.ino
@@ -1,9 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
+bool boo = 0;
 
+void setup() {
+Serial.begin(9600) // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+boo = !boo;
+Serial.println(boo);
+delay(200);
 }
